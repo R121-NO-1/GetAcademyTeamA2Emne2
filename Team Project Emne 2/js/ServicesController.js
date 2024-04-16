93e771c463fcf841e91148d5616a79d228557dee
Imlov3code <7042487@qq.com>
2024-04-16 20:03:49 +0200

    #levi commit
    fix updateview function error

--- Team Project Emne 2/js/ServicesController.js
+++ Team Project Emne 2/js/ServicesController.js
@@ -1,33 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const expandButtons = document.querySelectorAll('.expandButton');
-
-    expandButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const expandedContent = button.nextElementSibling;
-            if (expandedContent.style.display === 'none' || expandedContent.style.display === '') {
-                expandedContent.style.display = 'block';
-                button.classList.add('large'); // Add the 'large' class to make the button bigger
-                button.textContent = 'Collapse View';
-            } else {
-                expandedContent.style.display = 'none';
-                button.classList.remove('large'); // Remove the 'large' class to revert to original size
-                button.textContent = 'Expand View';
-            }
-        });
-    });
-
-    // part 2 of gallery
-    // joakim det er meg gjøre dette (levi)
-    const expandButton = document.getElementById('expandButton');
-    const expandedContent = document.getElementById('expandedContent');
-
-    expandButton.addEventListener('click', function() {
-        if (expandedContent.style.display === 'none') {
-            expandedContent.style.display = 'block';
-            expandButton.textContent = 'Collapse View';
-        } else {
-            expandedContent.style.display = 'none';
-            expandButton.textContent = 'Expand View';
-        }
-    });
+ 
 });
--- Team Project Emne 2/js/contactView.js
+++ Team Project Emne 2/js/contactView.js
@@ -1,29 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
     const app = document.getElementById('contact-info');
 
     function updateView() {
-        let contactForm = '';
+        const email = model.data.contactInfo.email;
+        const phone = model.data.contactInfo.phone_Number;
 
-        contactForm = /* "contact-info" HTML*/`
+        let contactForm = `
             <h2>Forespørsler håndteres innen én time</h2>
             <p> Bruk skjema, send mail eller ring oss direkte. 
             Alle forespørsler håndteres innen en time i arbeidstiden vår (man-fre, 8-16). </p>
-            <p> Telefon: +47 4568 88 888 </p> 
-
-            <p>${model.data.contactInfo.email}</p>
+            <p>${phone}</p>
+            <p>${email}</p>
         `;
         
         app.innerHTML = contactForm;
     }
 
-
-
-    
-
-
-
-    document.addEventListener('DOMContentLoaded', function() {
-        updateView();
-    })
-
-
-   
+    updateView();
+});
