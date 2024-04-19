c4be8cc5638dc40963762d24545495ded3bdffec
R121-NO-1 <157689074+R121-NO-1@users.noreply.github.com>
2024-04-19 14:20:51 +0200

    changes css

--- Team Project Emne 2/js/AboutView.js
+++ Team Project Emne 2/js/AboutView.js
@@ -1,31 +1,3 @@
-
-const app = document.getElementById('about_contact');
-
-function updateView() {
-    let contactForm = '';
-
-    contactForm = /* " about_contact" HTML*/`
-        <h2>Forespørsler håndteres innen én time</h2>
-        <p> Bruk skjema, send mail eller ring oss direkte. 
-        Alle forespørsler håndteres innen en time i arbeidstiden vår (man-fre, 8-16). </p>
-        <p> Telefon: +47 4568 88 888 </p> 
-
-        <p>${model.data.contactInfo.email}</p>
-    `;
-    
-    app.innerHTML = contactForm;
-}
-
-
-
-
-
-
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     updateView();
 })
