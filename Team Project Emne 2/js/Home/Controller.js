721c560d427ec141d8c482357001785c4d97f87d
Imlov3code <7042487@qq.com>
2024-05-02 13:55:08 +0200

    Revert "Merge branch 'main' of https://github.com/R121-NO-1/GetAcademyTeamA2Emne2"
    This reverts commit 45198c021e9eb624bd73e7ebc7ba4fa504aadf3d, reversing
    changes made to 2ecdaec72502235ca7fb19583de49a9ecf47fc69.

--- Team Project Emne 2/js/Home/Controller.js
+++ Team Project Emne 2/js/Home/Controller.js
@@ -3,7 +3,9 @@
 // DOMContentLoaded
 document.addEventListener('DOMContentLoaded', function() {
     let currentSlide = 0;
-    
+
+    // global variables
+    const thumbnails = document.querySelectorAll('.thumbnail');
     const galleryOverlay = document.querySelector('.gallery-overlay');
     const gallerySlides = document.querySelectorAll('.gallery-slides img');
     const viewMoreButton = document.getElementById('viewMore');
@@ -12,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = '../html/services.html';
     });
 
-    const thumbnails = document.querySelectorAll('.thumbnail');
     thumbnails.forEach(thumbnail => {
         thumbnail.addEventListener('click', function() {
             const index = parseInt(thumbnail.getAttribute('data-index'));
@@ -22,19 +23,9 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    document.querySelector('.close-btn').addEventListener('click', () => {
-        galleryOverlay.style.display = 'none';
-    });
-
-    document.querySelector('.prev-btn').addEventListener('click', () => {
-        currentSlide = (currentSlide - 1 + gallerySlides.length) % gallerySlides.length;
-        showSlide();
-    });
-
-    document.querySelector('.next-btn').addEventListener('click', () => {
-        currentSlide = (currentSlide + 1) % gallerySlides.length;
-        showSlide();
-    });
+    document.querySelector('.close-btn').addEventListener('click', hideGalleryOverlay);
+    document.querySelector('.prev-btn').addEventListener('click', prevSlide);
+    document.querySelector('.next-btn').addEventListener('click', nextSlide);
 
     function toggleThumbnailSize(thumbnail) {
         thumbnail.classList.toggle('large'); // Toggle 'large' class
@@ -45,16 +36,24 @@ document.addEventListener('DOMContentLoaded', function() {
         showSlide();
     }
 
+    function hideGalleryOverlay() {
+        galleryOverlay.style.display = 'none';
+    }
+
+    function prevSlide() {
+        currentSlide = (currentSlide - 1 + gallerySlides.length) % gallerySlides.length;
+        showSlide();
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % gallerySlides.length;
+        showSlide();
+    }
+
     function showSlide() {
         gallerySlides.forEach(slide => slide.style.display = 'none');
         gallerySlides[currentSlide].style.display = 'block';
     }
-
-    window.addEventListener('click', (event) => {
-        if (event.target === galleryOverlay) {
-            galleryOverlay.style.display = 'none';
-        }
-    });
 });
 
 
